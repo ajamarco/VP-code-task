443e10b15eb6a0dd8906027195bfe315917395cd
Alex Jamarco <a.jamarco@gmail.com>
2026-01-11 11:46:17 +0000

    Added the slice call to App.tsx file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,19 @@
+import { useAppDispatch, useAppSelector } from "../hooks/redux";
+import { increment } from "../features/counter/counterSlice";
+
 function App() {
+  const value = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
     <>
+      <div className="text-2xl font-bold">Counter: {value}</div>
+      <button
+        className="px-3 py-2 rounded bg-black text-white"
+        onClick={() => dispatch(increment())}
+      >
+        +1
+      </button>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
